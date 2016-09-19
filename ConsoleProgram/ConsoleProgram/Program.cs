fe5caba2eb5579ea6ffd71c7ecf8cf60c1c80839
CodeFarmer <631320085@163.com>
2016-09-20 00:05:32 +0800

    添加算法项目，添加滴滴出行秋招算法题

---
+++ ConsoleProgram/ConsoleProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleProgram.Puzzle;
+
+namespace ConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入要运行的算法：");
+            string algorithm = Console.ReadLine();
+            switch(algorithm.ToLower())
+            {
+                case "didi":
+                    Didi.Run();
+                    break;
+            }
+        }
+    }
+}
